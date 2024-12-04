d30a2074bbdc017aa6dbf17142ca24c73fa88085
AzyzXXIII <azizhcini001@gmail.com>
2024-12-04 11:00:24 +0100

    adding the API

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import accountReducer from "./Features/accounts/accountsSlice";
 import customerReducer from "./Features/customers/customerSlice";
+import { thunk } from "redux-thunk";
+
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
