993f63b16e6b84881159338fa8ddbc7e564624b6
AzyzXXIII <azizhcini001@gmail.com>
2024-12-04 19:54:17 +0100

    implementing the reduxToolKit

--- src/Features/accounts/accountsSlice.js
+++ src/Features/accounts/accountsSlice.js
@@ -1,11 +1,46 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
   balance: 1000,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
+const acocuntSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
+
+console.log(acocuntSlice);
+export const { deposit, withdraw, requestLoan, payLoan } = acocuntSlice.actions;
+
+export default acocuntSlice.reducer;
+
+/* export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -72,3 +107,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: "account/payLoan" };
 }
+ */
--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+/* import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension"; */
+
 import accountReducer from "./Features/accounts/accountsSlice";
 import customerReducer from "./Features/customers/customerSlice";
-import { thunk } from "redux-thunk";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: { account: accountReducer, customer: customerReducer },
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
